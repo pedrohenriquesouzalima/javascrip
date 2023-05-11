f267a6ef09d5fea06b4753818b421914c09af878
Pedro Henrique Souza Lima <pedrohenriquesolt@gmail.com>
2023-05-11 00:17:53 -0300

    atualização do desafio d002

--- desafios/d002/JS/script.js
+++ desafios/d002/JS/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/fotomanha.png'
+        document.body.style.background = '#dcdcc7'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagens/fototarde.png'
+        document.body.style.background = '#e3beac'
+    } else {
+        img.src = 'imagens/fotonoite.png'
+        document.body.style.background = '#111f27'
+    }
+}
+
